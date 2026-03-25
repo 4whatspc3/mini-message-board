f2e6420db6dbc733240506caa65071edce6d2a3a
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2026-03-25 18:25:41 -0300

    feat: add custom request logging middleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import indexRouter from './routes/indexRouter.js';
 import sendMessageRouter from './routes/sendMessageRouter.js';
+import logger from './middleware/logger.js';
 
 const app = express();
 
@@ -9,6 +10,8 @@ app.set("views", path.join(process.cwd(), "views"));
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({extended : true}));
+
+app.use(logger);
 
 app.use("/sendMessage", sendMessageRouter);
 
