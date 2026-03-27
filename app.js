5d03f8855e8525b5434693c26d789658cfaf32d0
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2026-03-27 13:21:04 -0300

    feat: configure express.static to serve files from the public directory
    Style the home page via 'styles.css'

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const app = express();
 app.set("views", path.join(process.cwd(), "views"));
 app.set("view engine", "ejs");
 
+const assetsPath = path.join(process.cwd(), "public");
+app.use(express.static(assetsPath));
+
 app.use(express.urlencoded({extended : true}));
 
 app.use(logger);
